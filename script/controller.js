85773aa473f319026c759db71e9bb62dfe91412e
Firepaw2106 <therese.signeboen@hotmail.no>
2025-01-13 14:53:30 +0100

    delite task done

--- script/controller.js
+++ script/controller.js
@@ -1,9 +1,33 @@
 function addTask(){
     const input = document.getElementById("taskInput").value;
 
+   if(input==="") {
+    alert("Task emety");
+
+   }
+   else{
     view.innerHTML += /*HTML*/`
     <div id= "listContainer">
-    <li><input type="checkbox">${input}</li>
+    <div id= "deliteIcon"onclick="deliteTask()">\u00d7</div>
+    <li id= "task" onclick= "checkToggle()">${input}</li>
+    
     </div>
     `;
+   }
+}
+
+function deliteTask(){
+
+    document.getElementById("listContainer").remove(this);
+}
+
+function checkToggle(){
+    let task = document.getElementById("task")
+
+    if(task.class =="checked"){
+        task.classList.remove("checked")
+    }else if(task.class == ""){
+        task.classList.add("checked")
+    }
+
 }
